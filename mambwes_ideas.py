7f2981a6fa95a697536674ea5127f3c20577332a
mambwensanje <nsanje9@gmail.com>
2026-02-28 11:48:12 +0800

    Added a space and comments between sections of my print statements

--- mambwes_ideas.py
+++ mambwes_ideas.py
@@ -3,17 +3,22 @@
 # I decided to include my ideas for web apps in here as well, just for shits and giggs
 
 print("These were my ideas:\n")
+
+# An app to review units
 print("1. A UWA Unit review/survival guide platform.") 
 print("Everytime I open reddit, I see about 4-5 new posts asking specifically about units, reviews, difficulty and tips.")
 print("Features: User accounts, Review system, Aggregated stats, Filter by dept, visualize workload vs rating.\n")
 
+# Something to review part-time jobs for students
 print("2. Part-time job review platform for students.")
 print("A platform where students can review retail jobs, workload, flexibility, Pay fairness & interview difficulty.")
 print("Features: Company profiles, anonymous reviews, rating breakdowns & 'best for students' ranking.\n")
 
+# How to track spending and budget effectively
 print("3. Student cost-of-living dashboard.")
 print("Again from what I've noticed on reddit, particularly applies to int'l student because they can't relate to the Aus economy.")
 print("Users can log expenses (rent, groceries, fuel, etc.), categorise spending, view a personal monthly breakdown & view anonymised community avg.\n")
 
+# Closing statement
 print("The ideas are all personal to me and my experience in uni so far, I'm open to hearing what you guys are passionate about and hopefully we can come up with even more ideas from there.")
 print("Have a great long-weekend and stay safe!")
