9fcf096f7376c4bb9da1b7bb5f134f357465455c
mambwensanje <nsanje9@gmail.com>
2026-02-28 11:43:18 +0800

    Mambwe's ideas and first commit using a py file

---
+++ mambwes_ideas.py
@@ -0,0 +1,19 @@
+# Hi guys, this is my first attempt at making a pull request on git/github.
+
+# I decided to include my ideas for web apps in here as well, just for shits and giggs
+
+print("These were my ideas:\n")
+print("1. A UWA Unit review/survival guide platform.") 
+print("Everytime I open reddit, I see about 4-5 new posts asking specifically about units, reviews, difficulty and tips.")
+print("Features: User accounts, Review system, Aggregated stats, Filter by dept, visualize workload vs rating.\n")
+
+print("2. Part-time job review platform for students.")
+print("A platform where students can review retail jobs, workload, flexibility, Pay fairness & interview difficulty.")
+print("Features: Company profiles, anonymous reviews, rating breakdowns & 'best for students' ranking.\n")
+
+print("3. Student cost-of-living dashboard.")
+print("Again from what I've noticed on reddit, particularly applies to int'l student because they can't relate to the Aus economy.")
+print("Users can log expenses (rent, groceries, fuel, etc.), categorise spending, view a personal monthly breakdown & view anonymised community avg.\n")
+
+print("The ideas are all personal to me and my experience in uni so far, I'm open to hearing what you guys are passionate about and hopefully we can come up with even more ideas from there.")
+print("Have a great long-weekend and stay safe!")
