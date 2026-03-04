9d264d13b44d96894ee46ed00eb1a3a6cb348fea
KGLapuz <lapuzkeithlin@gmail.com>
2026-03-04 17:25:52 +0800

    files added

---
+++ keithlins_ideas.py
@@ -0,0 +1,70 @@
+# These are my ideas for the project so far!
+
+#idea 1
+print('Commission organizer social media')
+'''specifically to help organize the commissions between clients and commissionee.
+
+Ive always noticed most commissionees only handle their commissions through their normal social media handle,
+making it harder for clients and commissionees to track the progress of their commission and often lead to numerous scams.
+to prevent this, ive thought of possible features we could make depending on what type of user is the account:
+
+commissionee:
+- they can create commission options, prices can be set and options can be archived for client reviews
+- commission options can be then be promoted in fyp
+
+clients:
+- they can leave comment and star reviews of a particular commissionee or commission option
+
+both:
+- they can communicate through direct messages
+- a commission progress tracker, updated by the commissionee.
+- transactions are available. 
+- both can have profiles. commissionee's a 'shop' while clients a 'dashboard' 
+- their own fyp to discover other commissionees. filtered with tags they can make.
+- report button
+'''
+
+#idea 2 
+print('sign language lingo')
+'''a take on duolingo's approach to learning foreign languages except this website is specific on sign language.
+
+Sign language isn't exactly common knowledge within our society. However, to some it is the only way they can communicate to us.
+By learning sign language we can close the communication gap in our diverse society allowing for awareness and inclusivity. 
+
+while I have never learnt this type of communication before, it sounds like a good opportunity to learn it along the way. if you guys dont mind it too.
+
+features:
+- a main mechanism of utilising hand gesture detection and pairing it with a library of existing sign languages
+- category of different sign languages (but we can focus on a specific one for this project and infer that other sign languages are thought of)
+- a reviewable gesture guide, including videos. 
+- feedback option 
+- leaderboard system
+- streak system
+'''
+
+#idea 3
+print('event planner')
+''' manage events between casual hangouts or large events.
+planning events in group chats whether you make new ones to specifically plan the event or use an existing chat can cause clutter in our dms
+and distruption to conversations. in personal past experience, I've often lost track of which group chats are temporary and even event plans are
+lost inside the sea of other conversations. hopefully making these features in the webapp could atleast alleviate this problem
+
+features:
+- user dashboard: personal calendar: where they can add which days they have available and existing events planned out,
+current chats and archived chats. They can also create chats and large event options here
+
+for small group hangouts:
+- in current chats, there will be dashboard option to show a calendar collecting of everyone's availability as theyve added in their own personal calendar,
+there will also be notes options so that users can customize it to add place, time, roles, etc
+- archived chats, everyone in the chat can contribute all their photos from the event, laid out in a grid gallery where users can pick and choose what to download.
+- friending system through qr code or friend through mutual event
+- chat invitation qr code or invitation through already existing friend.
+
+for large events:
+- large events: user must already thought out place, time to customize their invitation card. they can send this via qr code or existing friends.
+it is exclusively an invitation where people RSVP before the due date and leave comments such as dietary or contact if they are guest users
+- RSVP from large events: after due date, the website display how many attending guest and contacts of each for the host and even sends a summary through email
+- they will also have grid gallery for photos. 
+'''
+
+# thanks for reading my ideas! I can't wait to hear what you guys have thought for this project. 
