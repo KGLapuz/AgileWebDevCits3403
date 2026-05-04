e3c54836c0d47d7187b803dac13398d5b9a0bc32
mambwensanje <nsanje9@gmail.com>
2026-05-04 11:14:01 +0800

    Created comment boundary around my block in routes.py

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from fake_db import get_unit, discussions, get_user
 from flask import render_template
 
+# -----------------------------------------------------------------------------------------------------
 # MAMBWE trying out some functions in flask. DO NOT DELETE
 @app.route("/discussion/<int:discussion_id>")
 def discussion_thread(discussion_id):
@@ -20,3 +21,5 @@ def discussion_thread(discussion_id):
         get_user=get_user,   # pass function into Jinja
         current_user=current_user
     )
+    
+# -----------------------------------------------------------------------------------------------------
