6f093546edc665f023b5f11502b92c2275180fb5
mambwensanje <nsanje9@gmail.com>
2026-05-17 12:12:14 +0800

    Added back the log in js

---
+++ app/static/js/log_in_page.js
@@ -0,0 +1,17 @@
+// Function that changes the display of the SignUpForm to show it
+function popupSignup() {
+    document.getElementById("SignUpForm").style.display = "flex";
+}
+
+// Function that changes the display of the SignUpForm to hide it.
+function closeSignUp() {
+    document.getElementById("SignUpForm").style.display = "none";
+}
+
+// This is a function that closes the popup when the user clicks on the background
+window.onclick = function(event) {
+    let modal = document.getElementById("SignUpForm");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
